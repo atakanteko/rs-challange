bf81d6675e6b8fc52f9a6cbfa68c7d4ad738ff8e
atakantekoglu <atakantekoglu@gmail.com>
2022-07-21 14:43:37 +0300

    init rs challenge

---
+++ assets/static/navbar.js
@@ -0,0 +1,21 @@
+import home from '../icons/home-icon.svg'
+import order from '../icons/order-icon.svg'
+import settings from '../icons/settings-icon.svg'
+
+export const navbar = [
+  {
+    id: 0,
+    name: 'home',
+    icon: home
+  },
+  {
+    id: 1,
+    name: 'order',
+    icon: order
+  },
+  {
+    id: 2,
+    name: 'settings',
+    icon: settings
+  }
+]
---
+++ layouts/default.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="h-screen flex flex-row">
+    <Navbar/>
+    <Nuxt />
+  </div>
+</template>
+
+<script>
+
+
+export default {
+  name: "default",
+}
+</script>
+
+<style scoped>
+
+</style>
